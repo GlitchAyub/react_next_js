5f48bba948c6a34cb0c898d386db7deee13c611b
Ayub Thapa <ayub.phenx@gmail.com>
2024-04-19 17:44:47 +0530

    layouts done

---
+++ src/app/(group)/(grp_layout)/layout.tsx
@@ -0,0 +1,13 @@
+export default function GroupLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+
+    return(
+        <>
+        {children}
+        <h2>Layout Works</h2>
+        </>
+    )
+  }
