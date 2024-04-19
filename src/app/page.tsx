297a1053073fdc1322dd0eea66ce76620d775be3
Ayub Thapa <ayub.phenx@gmail.com>
2024-04-19 18:12:25 +0530

    tailwind added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-    return <h1>Home Page!</h1>
-}
+    return (
+ <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+
+ </>
+    )
+  }
