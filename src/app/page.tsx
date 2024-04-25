a320238978ae0dcc9ab0074b7e010a345cbe8824
Ayub Thapa <ayub.phenx@gmail.com>
2024-04-25 19:22:23 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link"
+
 export default function Home() {
-    return <h1>Home Page!</h1>
+    return (    <>
+      <h1>Home Page!</h1>
+     <Link href={"/products"}>Products</Link>
+     </>)
+
 }
